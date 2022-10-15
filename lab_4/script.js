69be67753b481210d3e7f9bb3b5b40e7472a9775
Anastasiiiii <nastia262002@gmail.com>
2022-10-15 14:06:32 +0300

    add 4th lab files and function to change color with click

---
+++ lab_4/script.js
@@ -0,0 +1,12 @@
+//let educationLine;
+
+function changeColor() {
+    document.getElementById("education").style.color = '#F2E3D5';
+    document.getElementById("education").style.backgroundColor = '#012E40';
+}
+
+
+function colorChange() {
+    document.querySelector("p.school").style.color = '#FFEEE6';
+    document.querySelector("p.school").style.backgroundColor = '#2A8277';
+}
